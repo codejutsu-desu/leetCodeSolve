8351f84a9ed5504c83462ba65c87215dc559341a
Code-Jutsu <mrimmoys@gmail.com>
2024-02-25 18:57:12 +0600

    acp

---
+++ 23_Roman_To_Integer.js
@@ -0,0 +1,27 @@
+function romanToInt(s) {
+  const romanNumerals = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let currentSymbolValue = romanNumerals[s[i]];
+    let nextSymbolValue = romanNumerals[s[i + 1]];
+
+    if (nextSymbolValue > currentSymbolValue) {
+      result += nextSymbolValue - currentSymbolValue;
+      i++;
+    } else {
+      result += currentSymbolValue;
+    }
+  }
+
+  return result;
+}
